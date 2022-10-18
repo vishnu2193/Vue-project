d309f0d9ea5f3522f70e31ddd281689be5ef8f3a
vishnu2193 <81963203+vishnu2193@users.noreply.github.com>
2022-10-18 08:38:28 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Vishnu",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
