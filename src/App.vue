709f7e5e7bc3a81384a16b912532cf596c582e39
sreddy39940 <somulavishnuvardhan.reddy@cengage.com>
2022-10-25 20:20:29 +0530

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <h1>{{name }}</h1>
-  <ChildVue/>
+  <!-- using if statemment hide/show the child component -->
+  <div v-if="showChild">
+<!-- sending props below -->
+<!-- another way to send props with binding ex:'Vis' -->
+  <ChildVue :Vis='Vis' Text="Another props"/>
+  </div>
+  <!-- calling method by onclick -->
+  <button @click="ChildOpen">ShowChildBtn</button>
 </template>
 
 <script>
@@ -11,8 +18,17 @@ export default {
   data() {
     return {
       name: 'Vishnu',
+      Vis:'Hello prop',
+      // below is flag to show/hide child component
+      showChild:false
     };
   },
+  methods:{
+    // below method will show/hide child component
+    ChildOpen(){
+      this.showChild= !this.showChild;
+    }
+  }
 };
 </script>
 
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -2,14 +2,19 @@
   <div>
 Hello child
   </div>
+  <!-- Access the props -->
+  <div>{{Vis}}</div>
+  <div>{{Text}}</div>
 </template>
 
 <script>
 export default {
+  // receiving props below
+  props:['Vis',"Text"],
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  // props: {
+  //   msg: String,
+  // },
 };
 </script>
 
